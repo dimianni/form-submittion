8dd799c412edd9a8ad908c2ba60ae931af870e18
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-05-28 12:15:35 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function () {
+    init()
+})
+
+function init() {
+
+    const form = document.getElementById('form')
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+
+        sendData()
+
+    })
+
+    function sendData(_this) {
+
+        const data = new FormData(_this)
+
+        fetch('https://dimianni.com/form/sendEmail.php', {
+            method: 'POST',
+            body: data
+        })
+            .then(res => {
+                console.log(res);
+                res.text()
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log("Error:", err))
+    }
+
+}
